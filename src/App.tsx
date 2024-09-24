300a456b326dcd6cb4f6a1e1e9c9a1446af63c6c
A <anotherstarburst@users.noreply.github.com>
2024-09-24 20:40:18 +0100

    Carves out WorkSurface - Plugs in google maps.

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,25 @@
-import { useMemo, useState } from "react";
-
-import responseOne from './api-responses/SE_State_Management_Polygons_1.json'
-import responseTwo from './api-responses/SE_State_Management_Polygons_2.json'
+import { SolutionProvider } from './SolutionProvider';
+import Solutions from './Solutions';
+import Statistics from './Statistics';
+import WorkSurface from './WorkSurface';
 
 function App() {
-  const [selectedSolution, setSelectedSolution] = useState<number>(1);
-
-  const payload = useMemo(() => {
-    if (selectedSolution === 1) return responseOne;
-    return responseTwo;
-  }, [selectedSolution]);
-
   return (
-    <div className="container-fluid">
-      <div className="row">
-        <div
-          className="col"
-          style={{
-            maxWidth: '200px',
-          }}
-        >
-          <h3>Solutions</h3>
-
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <input className="form-check-input me-2" type="radio" name="listGroupRadio" value="" id="firstRadio" checked={selectedSolution === 1}
-                onClick={() => {
-                  setSelectedSolution(1);
-                }}
-              />
-              <label className="form-check-label" htmlFor="firstRadio">Solution 1</label>
-            </li>
-            <li className="list-group-item">
-              <input className="form-check-input me-2" type="radio" name="listGroupRadio" value="" id="secondRadio" checked={selectedSolution === 2}
-                onClick={() => {
-                  setSelectedSolution(2);
-                }}
-              />
-              <label className="form-check-label" htmlFor="secondRadio">Solution 2</label>
-            </li>
-          </ul>
+    <SolutionProvider>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col" style={{ maxWidth: '200px' }}>
+            <Solutions />
+          </div>
+          <div className="col">
+            <WorkSurface />
+          </div>
+          <div className="col" style={{ maxWidth: '200px' }}>
+            <Statistics />
+          </div>
         </div>
-        <div className="col"><p>Work surface for a selected solution</p>
-          {JSON.stringify(payload)}
-        </div>
-        <div
-          className="col"
-          style={{
-            maxWidth: '200px',
-          }}
-        >
-          <h3>Statistics</h3>
-          <hr />
-          <h3>Tools</h3>
-          <hr />
-          Tools available for selected solution
-        </div>
-      </div >
-    </div >
+      </div>
+    </SolutionProvider>
   );
 }
 
