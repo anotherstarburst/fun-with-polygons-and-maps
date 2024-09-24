7dddedb5d8ff6e8b2a33c79e8e29e02c2cd695a7
A <anotherstarburst@users.noreply.github.com>
2024-09-24 20:39:33 +0100

    Adds solution provider.

---
+++ src/SolutionProvider.tsx
@@ -0,0 +1,25 @@
+import { useState, useMemo, ReactNode } from 'react';
+import responseOne from './api-responses/SE_State_Management_Polygons_1.json';
+import responseTwo from './api-responses/SE_State_Management_Polygons_2.json';
+import { SolutionContext } from './SolutionContext';
+import { FeatureCollection } from './types';
+
+interface SolutionProviderProps {
+  children: ReactNode;
+}
+
+export function SolutionProvider({ children }: SolutionProviderProps) {
+  const [selectedSolution, setSelectedSolution] = useState<number>(1);
+
+  const payload = useMemo(() => {
+    return selectedSolution === 1 ? responseOne : responseTwo;
+  }, [selectedSolution]) as FeatureCollection;
+
+  return (
+    <SolutionContext.Provider
+      value={{ selectedSolution, setSelectedSolution, payload }}
+    >
+      {children}
+    </SolutionContext.Provider>
+  );
+}
