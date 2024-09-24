723b20b6fc2b1f5380b78075e96e00281374a29a
A <anotherstarburst@users.noreply.github.com>
2024-09-24 20:38:05 +0100

    Adds SolutionContext, to avoid prop drilling.

---
+++ src/SolutionContext.ts
@@ -0,0 +1,23 @@
+import { Dispatch, SetStateAction, createContext, useContext } from 'react';
+import { FeatureCollection } from './types';
+
+
+interface SolutionContextType {
+    selectedSolution: number;
+    setSelectedSolution: Dispatch<SetStateAction<number>>;
+    payload: FeatureCollection;
+}
+
+export const SolutionContext = createContext<SolutionContextType | undefined>(
+    undefined
+);
+
+export function useSolutionContext() {
+    const context = useContext(SolutionContext);
+    if (!context) {
+        throw new Error(
+            'useSolutionContext must be used within a SolutionProvider'
+        );
+    }
+    return context;
+}
