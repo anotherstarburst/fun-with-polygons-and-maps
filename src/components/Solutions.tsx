591080583d6f43013963798a72f8f8645ab14423
A <anotherstarburst@users.noreply.github.com>
2024-09-25 18:03:46 +0100

    Refactor: Solutions panel - allow for arbitrary number of solutions

--- src/components/Solutions.tsx
+++ src/components/Solutions.tsx
@@ -4,15 +4,35 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLayerGroup } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
-const Solutions: React.FC = () => {
-  const {
-    selectedSolutionIndex,
-    setSelectedSolutionIndex,
-  } = useSolutionContext();
+interface SolutionItemProps {
+  label: string;
+  index: number;
+}
 
-  const handleSolutionChange = (solutionIndex: number) => {
-    setSelectedSolutionIndex(solutionIndex);
-  };
+const SolutionItem: React.FC<SolutionItemProps> = (props) => {
+  const { selectedSolutionIndex, setSelectedSolutionIndex } =
+    useSolutionContext();
+  const { label, index } = props;
+
+  return (
+    <li className="list-group-item">
+      <input
+        className="form-check-input me-2"
+        type="radio"
+        name="listGroupRadio"
+        id={`radio${index}`}
+        checked={selectedSolutionIndex === index}
+        onChange={() => setSelectedSolutionIndex(index)}
+      />
+      <label className="form-check-label" htmlFor={`radio${index}`}>
+        {label}
+      </label>
+    </li>
+  );
+};
+
+const Solutions: React.FC = () => {
+  const { solutions } = useSolutionContext();
 
   return (
     <div className="d-flex flex-column h-100 justify-content-between">
@@ -22,32 +42,13 @@ const Solutions: React.FC = () => {
           Solutions
         </p>
         <ul className="list-group list-group-flush">
-          <li className="list-group-item">
-            <input
-              className="form-check-input me-2"
-              type="radio"
-              name="listGroupRadio"
-              id="firstRadio"
-              checked={selectedSolutionIndex === 0}
-              onChange={() => handleSolutionChange(0)}
+          {solutions.map((_, index) => (
+            <SolutionItem
+              key={`solution-${index}`}
+              label={`Solution ${index + 1}`}
+              index={index}
             />
-            <label className="form-check-label" htmlFor="firstRadio">
-              Solution 1
-            </label>
-          </li>
-          <li className="list-group-item">
-            <input
-              className="form-check-input me-2"
-              type="radio"
-              name="listGroupRadio"
-              id="secondRadio"
-              checked={selectedSolutionIndex === 1}
-              onChange={() => handleSolutionChange(1)}
-            />
-            <label className="form-check-label" htmlFor="secondRadio">
-              Solution 2
-            </label>
-          </li>
+          ))}
         </ul>
       </div>
       <a
